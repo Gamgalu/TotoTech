4d384df65d02bab1cda7af8e4b5a13ae621fc899
ngjoni <ngj@posteo.de>
2019-05-16 17:02:37 +0200

    included static files and index.html example (appShirt)

--- appShirt/views.py
+++ appShirt/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the test index.")
+    return render(request, 'appShirt/index.html')
