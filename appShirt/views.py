88f782e6facfd5cf577063300bd1234c7cbcd8f6
ngjoni <ngj@posteo.de>
2019-05-16 17:21:47 +0200

    posenet

--- appShirt/views.py
+++ appShirt/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'appShirt/index.html')
+
+def posenet(request):
+    return render(request, 'appShirt/posenet.html')
+
