bbc78ca49e211c9bf2cb7f617ac6c19ea992d957
Hauke <hauke.diers@outlook.com>
2019-05-17 07:16:01 +0200

    Resolve merge conflicts for speech api

--- appShirt/static/appShirt/speechapi.js
+++ appShirt/static/appShirt/speechapi.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const recognition = new webkitSpeechRecognition();
 
 let finalTranscript = '';
@@ -15,68 +15,12 @@ var auswahl = function (myParam) {
     } else {
         $("#weiterdiv").hide();
     }
-};
+}
 
 var weiter = function () {
     console.log("i was hessre");
     $("#weiterdiv").click();
-=======
-// first we make sure annyang started succesfully
-if (annyang) {
-
-    var pullover = function () {
-        alert('Pullover!');
-        console.log('Pullover!');
-        $.select("pullover");
-    };
-
-    var auswahl = function (myParam) {
-        // alert('Auswahl: ' + myParam);
-        console.log(myParam);
-        $.select(myParam);
-        if($.countgreen("") > 0) {
-            $("#weiterdiv").show();
-        } else {
-            $("#weiterdiv").hide();
-        }
-    };
-
-    var weiter = function () {
-         console.log("i was here");
-        $("#weiterdiv").click();
-    }
-
-    var zeigMir = function (nummer) {
-        console.log(nummer)
-    }
-
-    // List of all Commands
-    var commands = {
-        // 'hallo': hallo,
-        'pullover': function() {  auswahl('pullover'); },
-        'shirt': function() {  auswahl('thsirt'); },
-        'schlafshirt': function() {  auswahl('schlafshirt'); },
-        'poloshirt': function() {  auswahl('poloshirt'); },
-        'anzug': function() {  auswahl('anzug'); },
-        'jacken': function() {  auswahl('jacken'); },
-        'polo': function() {  auswahl('polo'); },
-        'sweatshirt': function() {  auswahl('sweatshirt'); },
-        'sakko': function() { auswahl('sakko'); },
-        'weiter': function() { weiter(); },
-    };
-
-    // Add our commands to annyang
-    annyang.addCommands(commands);
-
-    // Change language to german
-    annyang.setLanguage('de-DE');
-
-    // Start listening.
-    annyang.start();
-    console.log('Annyang started');
->>>>>>> 7f8ab2e138c634ee6b4559c1348df3aa4cc17e5f
 }
-
 
 recognition.onend = function() {
   if (finalTranscript !== '') {
@@ -123,7 +67,6 @@ recognition.onend = function() {
   //loop:
   recognition.start();
 };
-
 
 recognition.onresult = function(event) {
   let interimTranscript = '';
